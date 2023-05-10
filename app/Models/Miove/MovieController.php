71d25b8ad7a280e01a4813aed754be5d01fa41f2
manuelnones <manuelnones99@yahoo.com>
2023-05-10 16:28:26 +0200

    create model Movie + add nav-bar

---
+++ app/Models/Miove/MovieController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Miove;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MovieController extends Model
+{
+    use HasFactory;
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/main')
+
+@section('component')
+    <div class="container">
+        prova
+    </div>
+@endsection
